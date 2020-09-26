7210403fe7aca8626aa517834f4e78ffe2b7021c
TaeminMoon <tmmoond8@gmail.com>
2020-09-26 10:47:10 +0900

    Fix: temp logic

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,26 +1,16 @@
 import expressLoader from './express';
 import Logger from './logger';
 import typeorm from './typeorm';
-import User, { createUser } from '../entity/User';
 //We have to import at least all the events once so they can be triggered
 
 export default async (app: any) => {
+  Logger.info('✌️ Dependency Injector loaded');
   try {
-    const dbConnection = await typeorm();
-    const user = createUser({
-      name: 'ken',
-      email: 'test@test.com',
-      provider: 'google',
-      snsId: '23131321',
-    });
-    console.log(dbConnection, user);
-    const kk = await dbConnection.getRepository(User).save(user);
-    console.log(kk);
+    await typeorm();
+    Logger.info('✌️ MySQL connected');
   } catch (error: unknown) {
     console.log(error);
   }
-
-  Logger.info('✌️ Dependency Injector loaded');
 
   await expressLoader({ app });
   Logger.info('✌️ Express loaded');
