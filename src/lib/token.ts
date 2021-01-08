7c7dd280ef59d4349240e37c2570649186009895
tmmoond8 <tmmoond8@gmail.com>
2021-01-09 01:22:36 +0900

    Fix: 도메인 변경에 따른 코드 변경

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -60,7 +60,7 @@ export const jwtMiddleware = async (
 
 export const setCookie = (req: express.Request, res: express.Response, token: string) => {
   const url = urlParse(req.headers.referer || '');
-  const domain = url.hostname.replace('owner', '')
+  const domain = url.hostname.replace('www', '')
   res.cookie('access_token', token, {
     maxAge: 1000 * 60 * 60 * 24 * 7,
     domain
