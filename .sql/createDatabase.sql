8e9915da26d4a0d4d31a32d26103c21ea4aa2ce7
tamm <tmmoond8@gmail.com>
2021-05-28 20:55:32 +0900

    feat: 배포 설정 변경

--- .sql/createDatabase.sql
+++ .sql/createDatabase.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA picar_db;
-CREATE USER 'picar'@'localhost' IDENTIFIED BY 'dhsjtm';
-CREATE USER 'picar'@'%' IDENTIFIED BY 'dhsjtm';
+CREATE USER 'picar'@'localhost' IDENTIFIED BY 'vlzk';
+CREATE USER 'picar'@'%' IDENTIFIED BY 'vlzk';
 GRANT ALL PRIVILEGES ON picar_db.* TO 'picar'@'localhost';
 GRANT ALL PRIVILEGES ON picar_db.* TO 'picar'@'%' WITH GRANT OPTION;
