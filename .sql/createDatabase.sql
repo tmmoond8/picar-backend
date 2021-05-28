4b2fb440f911e3cbdab51dfba495c28662f88c11
tamm <tmmoond8@gmail.com>
2021-05-28 20:59:05 +0900

    feat: mysql 비번 설정값 변경

--- .sql/createDatabase.sql
+++ .sql/createDatabase.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA picar_db;
-CREATE USER 'picar'@'localhost' IDENTIFIED BY 'vlzk';
-CREATE USER 'picar'@'%' IDENTIFIED BY 'vlzk';
+CREATE USER 'picar'@'localhost' IDENTIFIED WITH mysql_native_password BY 'vlzk';
+CREATE USER 'picar'@'%' IDENTIFIED WITH mysql_native_password BY 'vlzk';
 GRANT ALL PRIVILEGES ON picar_db.* TO 'picar'@'localhost';
 GRANT ALL PRIVILEGES ON picar_db.* TO 'picar'@'%' WITH GRANT OPTION;
