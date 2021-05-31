1b19630708f484cb776d1bf8afae5b8343de570b
tamm <tmmoond8@gmail.com>
2021-05-31 22:55:21 +0900

    feat: 12시간 마다 크론 처리

--- src/cronJop/index.ts
+++ src/cronJop/index.ts
@@ -1,7 +1,17 @@
 import newsFeed from './newsFeed';
 
+const cronJob = (hours: number, job: () => void) => {
+  const scheduleJob = () => {
+    job();
+    setTimeout(() => {
+      scheduleJob();
+    }, hours * 1000)
+  }
+  scheduleJob();
+}
+
 const cron = () => {
-  newsFeed.parse();
+  cronJob(12, newsFeed.parse);
 };
 
 export default cron;
